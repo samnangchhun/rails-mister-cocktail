1387cf53591cac0b4f68ece9a6b98d306dae8eaf
Samnang Chhun <slowmoonstudio@gmail.com>
2021-03-05 12:34:14 +0100

    Bundler fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'open-uri'
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredient_data = open(url).read
+# ingredient = JSON.parse(ingredient_data)
+
+# ingredient["drinks"].each do |i|
+#   new_ingredient = Ingredient.create(name: i["strIngredient1"])
+#   puts " #{i.name}"
+# end
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice cubes")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "white sugar")
+Ingredient.create(name: "white rum")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "club soda")
